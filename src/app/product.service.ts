0fd9fca7af7f047466a58cbd537c9d3e375cab96
rgongora <rgongora@users.noreply.github.com>
2020-03-13 17:19:12 +0800

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -20,6 +20,6 @@ export class ProductService {
 
   getProducts() : Observable<Product[]> {
     return this._http.get(this._productsUrl)
-      .map(response => <Product[]> response.json());
+      .map(response => <Product[]>response.json());
   }
 }
